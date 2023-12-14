707bb3abed07cabd78e70537c1a5723d1749b766
JwlSdg <153847951+JwlSdg@users.noreply.github.com>
2023-12-14 11:34:32 -0500

    Update streamlit_app.py
    Diner stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
 
-streamlit.title('This is a title.')
-streamlit.header('This is a header.')
-streamlit.text('This is text.')
+
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
